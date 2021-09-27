e1a46e7488a24cc4edb76108e89ded35b99d874e
卢煜 <luyu.dashing@bytedance.com>
2021-09-27 09:29:52 +0800

    调试js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import './study/_04_datatype'
-import './study/_06_interface'
+// import './study/_06_interface'
 
+const a: number = require('./index_play.js')
 let hello: string = 'Hello TypeScript'
 document.querySelectorAll('.app')[0].innerHTML = hello;
---
+++ src/index_play.js
@@ -0,0 +1,69 @@
+
+// let o = {
+//   // get a() { return 1 },
+//   set a(x) { console.log(x); }
+// }
+// console.log(o.a)
+// o.a = 100
+// o.a = '200'
+
+// console.log(Object.getOwnPropertyDescriptor(o, 'a'));
+
+var o = new Object;
+var n = new Number;
+var s = new String;
+var b = new Boolean;
+var d = new Date;
+var arg = function () { return arguments }();
+var r = new RegExp;
+var f = new Function;
+var arr = new Array;
+var e = new Error;
+console.log(
+  [
+    o, n, s, b, d, arg, r, f, arr, e
+  ].map(
+    v => Object.prototype.toString.call(v)
+  )
+);
+
+function c1() { }
+console.log(c1.prototype)
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sperk() {
+    console.log(this.name + ' make a noise.');
+  }
+}
+
+console.log(Animal.prototype)
+
+let aAnimal = new Animal('red')
+console.log(aAnimal);
+
+function sleep(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, duration);
+  })
+}
+
+sleep(1000).then((e) => { console.log('finish', e); })
+// Promise 的resovle 永远是异步执行的
+
+async function foo(name) {
+  await sleep(2000)
+  console.log(name);
+}
+
+async function foo2() {
+  await foo('a')
+  await foo('b')
+}
+
+foo2()
+
+module.exports = 1
